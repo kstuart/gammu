38a1f1274d9169ad6df377c3eed5710d3471bd4b
Kenny Stuart <kstuart@hotmail.co.uk>
2019-06-12 08:52:02 +0100

    update postgres upgrade ddl

--- docs/sql/pgsql-upgrade.sql
+++ docs/sql/pgsql-upgrade.sql
@@ -8,9 +8,9 @@
 --
 alter table inbox drop constraint "inbox_Coding_check";
 alter table inbox add constraint "inbox_Coding_check"
-  check ("Coding" IN ('Network_Default', '7bit_ASCII', 'Default_No_Compression', 'Unicode_No_Compression', '8bit', 'Default_Compression', 'Unicode_Compression'));
+  check ("Coding" IN ('Default_No_Compression', 'Unicode_No_Compression', '8bit', 'Default_Compression', 'Unicode_Compression', '7bit_ASCII'));
 
 alter table outbox drop constraint "outbox_Coding_check";
 alter table outbox add constraint "outbox_Coding_check"
-  check ("Coding" IN ('Network_Default', '7bit_ASCII', 'Default_No_Compression', 'Unicode_No_Compression', '8bit', 'Default_Compression', 'Unicode_Compression'));
+  check ("Coding" IN ('Network_Default', 'Default_No_Compression', 'Unicode_No_Compression', '8bit', 'Default_Compression', 'Unicode_Compression', '7bit_ASCII'));
 
